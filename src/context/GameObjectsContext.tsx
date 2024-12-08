0dc45a6e5c0ca7b7293ef05c6cf612d287d2a186
arekalov <artyom.rekalov@gmail.com>
2024-12-08 12:14:58 +0300

    update sprites (remove tube)

--- src/context/GameObjectsContext.tsx
+++ src/context/GameObjectsContext.tsx
@@ -11,7 +11,7 @@ import {
     useMemo
 } from "react";
 import { RAIL_WIDTH, RAIL_X_LEFT } from "../objects/constants.ts";
-import rel from '../assets/rel.png'
+import rel from '../assets/rel.svg'
 import opticalGate from '../assets/optical_door.png'
 import pumpOn from '../assets/pump_on.png'
 import pumpOff from '../assets/pump_off.png'
