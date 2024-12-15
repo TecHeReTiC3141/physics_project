c96220ba630f9512a185738cd61497fc4c886334
arekalov <artyom.rekalov@gmail.com>
2024-12-15 17:17:42 +0300

    fix logic fun for third table

--- src/utils/logic/thirdTable.ts
+++ src/utils/logic/thirdTable.ts
@@ -22,10 +22,30 @@ export function normalizeGatesForThirdTable(
 export function calculateThirdTableDto(leftGate: number, rightGate: number): SecondTableDto {
     // Нормализуем gates
     const { normalizedLeftGate, normalizedRightGate } = normalizeGatesForThirdTable(leftGate, rightGate);
+    let t1 = 1.3
+    let t2 = 2.5
+    if (normalizedLeftGate === .4) {
+        t1 = 1.3
+        t2 = 2.5
+    } else if (normalizedLeftGate === .5) {
+        t1 = 1.2
+        t2 = 3.1
+    } else if (normalizedLeftGate === .7) {
+        t1 = 1.3
+        t2 = 3.5
+    } else if (normalizedLeftGate === .9) {
+        t1 = 1.3
+        t2 = 4.1
+    } else if (normalizedLeftGate === 1.1) {
+        t1 = 1.4
+        t2 = 4.6
+    } else {
+        throw new Error(`Unexpected block count for third table`)
+    }
 
     const randomOffset = () => (-.3 + Math.random() * 0.6);
-    const t1New = normalizedLeftGate + randomOffset();  // Значение t1 с добавлением случайного смещения
-    const t2New = normalizedRightGate + randomOffset();  // Значение t2 с добавлением случайного смещения
+    const t1New = t1 + randomOffset();  // Значение t1 с добавлением случайного смещения
+    const t2New = t2 + randomOffset();  // Значение t2 с добавлением случайного смещения
 
     // Вычисление отклонения
     const deviation = (t1New ** 2 - t2New ** 2) / 2;
